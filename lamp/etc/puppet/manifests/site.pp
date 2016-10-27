46beeec5bbd3c4942251991e8013b67341c06bb9
Andrii Bondar <andy.bondar@gmail.com>
2016-10-27 09:18:36 -0500

    Added php-mysql package, added mysql-test.php to check MySQL working

--- lamp/etc/puppet/manifests/site.pp
+++ lamp/etc/puppet/manifests/site.pp
@@ -1,10 +1,9 @@
 node default {
         $serveremail = 'webmaster@example.com'
         $servername = 'example.com'
-	$module_mysql = 'puppetlabs-mysql'
 
 	include apache
 	include apache::vhosts
 	include php
-	include mysql::server
+	include mysql::database
 }
